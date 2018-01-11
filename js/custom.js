271936ac697be714c3eafba017c3fc2ae55d1259
Abdul Mumin <mumin.rakib@yahoo.com>
2018-01-11 14:09:30 +0600

    Onu 00

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$grid = $('.grid').isotope({
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+
+$grid = $('.grid').isotope({
+  // options
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
+
+
+$('.filter button').on("click", function () {
+  var value = $(this).attr('data-name');
+    $grid.isotope({
+      filter: value
+    });
+})
+
+
+
+// My isotope srcipt starts heres
+// init Isotope
+
+
